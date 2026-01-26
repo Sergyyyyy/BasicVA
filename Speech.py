7ec41bf21dcf0596ec31f83da06ab85b5f6e2b1e
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-01-26 16:24:51 +0800

    Added new lighter  model

--- Speech.py
+++ Speech.py
@@ -1,26 +1,15 @@
+from vosk import Model, KaldiRecognizer
 import sounddevice as sd
 import json
-from vosk import Model, KaldiRecognizer
 
-model = Model("vosk-model-en-us-0.22-lgraph")
-recognizer = KaldiRecognizer(model, 16000)
+model = Model("Python/BasicVA/BasicVA/vosk-model-small-en-us-0.15")
+rec = KaldiRecognizer(model, 16000)
 
-def listen():
-    with sd.RawInputStream(
-        samplerate=16000,
-        blocksize=8000,
-        dtype="int16",
-        channels=1
-    ) as stream:
+def callback(indata, frames, time, status):
+    if rec.AcceptWaveform(indata):
+        print(json.loads(rec.Result()))
 
-        print("Listening...")
-
-        while True:
-            data, _ = stream.read(4000)
-
-            if recognizer.AcceptWaveform(data):
-                result = json.loads(recognizer.Result())
-                return result.get("text", "")
-
-spoken_text = listen()
-print("You said:", spoken_text)
+with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
+                       channels=1, callback=callback):
+    print("Listening...")
+    input()
