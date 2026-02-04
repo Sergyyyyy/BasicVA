4ca52f234eba958bed57c0d6b75ee03776ba6e06
sergio penalosa <penalosa.sergiobono@gmail.com>
2026-02-04 08:16:08 +0800

    Added basic speech recog functionality

--- Speech.py
+++ Speech.py
@@ -6,10 +6,17 @@ model = Model("Python/BasicVA/BasicVA/vosk-model-small-en-us-0.15")
 rec = KaldiRecognizer(model, 16000)
 
 def callback(indata, frames, time, status):
-    if rec.AcceptWaveform(indata):
-        print(json.loads(rec.Result()))
+    if status:
+        print(status)
+
+    if rec.AcceptWaveform(bytes(indata)):
+        result = json.loads(rec.Result())
+        print("You said:", result["text"])
+
+
 
 with sd.RawInputStream(samplerate=16000, blocksize=8000, dtype='int16',
                        channels=1, callback=callback):
     print("Listening...")
     input()
+
